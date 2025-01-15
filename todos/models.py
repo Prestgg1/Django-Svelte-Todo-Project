97169312079314773f03ed63b2afda9355b19265
Prestgg1 <prestgg12@gmail.com>
2025-01-15 12:36:24 +0400

    Todos Model Created, Todos Model has includes title,description,author,due_date,is_finished field

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from users.models import CustomUser
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='todos')
+    due_date = models.DateField()
+    is_finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
